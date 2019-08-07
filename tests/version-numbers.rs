f77871ce51f167baded1b1e57dc2fddc6d992672
Oleksii Filonenko <brightone@protonmail.com>
2019-08-07 13:32:48 +0300

    Add version-sync

---
+++ tests/version-numbers.rs
@@ -0,0 +1,11 @@
+#[test]
+#[ignore]
+// TODO: add versions to README.md
+fn test_readme_deps() {
+    version_sync::assert_markdown_deps_updated!("README.md");
+}
+
+#[test]
+fn test_html_root_url() {
+    version_sync::assert_html_root_url_updated!("src/lib.rs");
+}
