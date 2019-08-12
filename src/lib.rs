2cb7c80af0ab7b5dcffcddcb369e4db2d7fcec20
Oleksii Filonenko <brightone@protonmail.com>
2019-08-12 12:05:36 +0300

    (cargo-release) version 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/rusteam/0.4.6-rc.2")]
+#![doc(html_root_url = "https://docs.rs/rusteam/0.5.0-rc.2")]
 
 use crate::filesystem::entries;
 use crate::game::Game;
